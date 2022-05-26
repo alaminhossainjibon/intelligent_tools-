5bcc5bc5cb54d4b0448de84bbac15ecbf5fbf8c9
alaminhossainjibon <mdalamin7622@gmail.com>
2022-05-26 18:52:20 +0600

    my abouts

--- src/components/Home/About/About.js
+++ src/components/Home/About/About.js
@@ -1,9 +1,40 @@
 import React from 'react';
+import profile from '../../../assets/images/profile/profile.png'
 
 const About = () => {
     return (
-        <div>
-            <h2>This is about!!</h2>
+        <div class="hero min-h-screen bg-neutral m-12 rounded-lg shadow-lg mx-auto w-1/2">
+            <div class="p-6">
+                <img class="mask mask-circle w-60" src={profile} />
+                <div>
+                    <h1 class="text-5xl font-bold text-purple-600">@ My About!</h1>
+                    <p class="py-6">My name is Al-amin hossain, I'm from Bangladesh, I live in Rangpur. My goal and dreams full stack web developer. I learn programming and web development in programming-hero website.I love learning new technologies every day</p>
+                    <br />
+                    <h1 class="text-2xl font-bold text-purple-600">@ Everything I learned:</h1>
+                    <br />
+                    <p>
+                        # 1. HTML.
+                        <br />
+                        # 2. CSS
+                        <br />
+                        # 2. JavaScript
+                        <br />
+                        # 3. Node js
+                        <br />
+                        # 4. Database
+                        <br />
+                        -----------Framework-----------
+                        <br />
+                        # 1. Bootstrap
+                        <br />
+                        # 2. tailwindcss
+                        <br />
+                        <h1 class="text-2xl font-bold text-purple-600">My Learning Research!</h1>
+                        Programming Hero..
+                        www.programming-hero.com
+                    </p>
+                </div>
+            </div>
         </div>
     );
 };
